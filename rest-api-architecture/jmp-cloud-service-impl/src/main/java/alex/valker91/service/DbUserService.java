68d4a02f44bb1d12d6337a5f4a4a96f725877e2f
Aliaksandr_Kreyer <Aliaksandr_Kreyer@epam.com>
2025-09-14 15:08:47 +0300

    Update the app

---
+++ rest-api-architecture/jmp-cloud-service-impl/src/main/java/alex/valker91/service/DbUserService.java
@@ -0,0 +1,36 @@
+package alex.valker91.service;
+
+import alex.valker91.model.Subscription;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DbUserService implements SubscriptionService {
+
+    @Override
+    public Subscription create(Subscription subscription) {
+        return null;
+    }
+
+    @Override
+    public Optional<Subscription> update(Subscription subscription) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        return false;
+    }
+
+    @Override
+    public Optional<Subscription> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Subscription> findAll() {
+        return List.of();
+    }
+}
